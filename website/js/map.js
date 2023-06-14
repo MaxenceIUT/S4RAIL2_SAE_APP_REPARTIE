7294d331ae2666994aae03bfbeb78f92366eee3b
TanguyFox <tanguyfox@gmail.com>
2023-06-14 15:02:51 +0200

    Init website

---
+++ website/js/map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([48.68895663631991, 6.178244621722987], 14);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
