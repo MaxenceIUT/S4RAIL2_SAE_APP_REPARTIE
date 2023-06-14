55b8cddf4ae00773311cd89294d569cefec07237
perez112u <perez.ioann5@etu.univ-lorraine.fr>
2023-06-14 17:55:18 +0200

    ajt recuperer coordonnées

--- Java/AccesBDD.java
+++ Java/AccesBDD.java
@@ -1,44 +1,47 @@
+import org.json.*;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.*;
 
 public class AccesBDD implements ServiceBDD {
     @Override
-    public void recupererCoordonnees() {
+    public String recupererCoordonnees() throws SQLException {
 
+        Connection connection = DBConnection.getConnexion();
 
+        Statement st = connection.createStatement();
+        ResultSet res = st.executeQuery("SELECT name, latitude,longitude FROM goodfood_restaurants;");
+
+        JSONArray json = new JSONArray();
+        JSONObject json2;
+
+        while (res.next()) {
+            json2 = new JSONObject();
+            json2.put("nom", res.getString(1));
+            json2.put("latitude", res.getString(2));
+            json2.put("longitude", res.getString(3));
+            json.put(json2);
+        }
+
+        return json.toString();
     }
 
     @Override
     public void reserverTable(String nom, String prenom, String nb, String tel) {
-
+    throw new Error();
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException, RemoteException {
 
-//        AccesBDD acc = new AccesBDD();
-//        ServiceBDD sb = (ServiceBDD) UnicastRemoteObject.exportObject(acc, 0);
-//
-//        Registry reg = null;
+        AccesBDD acc = new AccesBDD();
+        ServiceBDD sb = (ServiceBDD) UnicastRemoteObject.exportObject(acc, 0);
 
-        // chargement driver
-        System.out.println("Driver loaded");
+        Registry reg = LocateRegistry.createRegistry(1099);
+        reg.rebind("bdd", sb);
 
-        // etablir connection
-        Connection connection = DriverManager.getConnection("jdbc:postgresql://db.xsmbkjnrbdkmorujjhtk.supabase.co:5432/postgres","postgres", "JBRVB8YpDClwzNF7@#jAGQY#K@b2mh^G&46");
-        System.out.println("Data connected");
-
-        Statement st = connection.createStatement();
-
-        ResultSet res = st.executeQuery("SELECT * FROM Restaurant;");
-
-        System.out.println("Resultat :");
-        while (res.next())
-            System.out.println(res.getString(1) + "\t" +
-                    res.getString(2) + "\t" + res.getString(3));
-        connection.close();
-
+        System.out.println("Service BDD lance");
 
 
     }
--- Java/ServiceBDD.java
+++ Java/ServiceBDD.java
@@ -1,8 +1,12 @@
+import org.json.JSONObject;
+
 import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.sql.SQLException;
 
 public interface ServiceBDD extends Remote {
 
-    public void recupererCoordonnees();
+    public String recupererCoordonnees() throws SQLException, RemoteException;
 
-    public void reserverTable(String nom, String prenom, String nb, String tel);
+    public void reserverTable(String nom, String prenom, String nb, String tel) throws RemoteException;
 }
