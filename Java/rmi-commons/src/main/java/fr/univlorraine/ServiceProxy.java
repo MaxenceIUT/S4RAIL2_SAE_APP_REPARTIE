c59626b11ac2b6ddce95dd6a562a0fa1e86905a3
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2023-06-15 14:19:03 +0200

    fix: illegal rmi method

--- Java/rmi-commons/src/main/java/fr/univlorraine/ServiceProxy.java
+++ Java/rmi-commons/src/main/java/fr/univlorraine/ServiceProxy.java
@@ -1,11 +1,12 @@
 package fr.univlorraine;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface ServiceProxy extends Remote {
 
-    void registerGoodFoodProvider(ServiceGoodFood provider);
+    void registerGoodFoodProvider(ServiceGoodFood provider) throws RemoteException;
 
-    void registerEtablissementsProvider(ServiceEtablissements provider);
+    void registerEtablissementsProvider(ServiceEtablissements provider) throws RemoteException;
 
 }
