759844f611421945f53bc1856f66aaa671861595
TanguyFox <tanguyfox@gmail.com>
2023-06-15 14:57:37 +0200

    Refactor javascript

--- website/js/data_collector.js
+++ website/js/data_collector.js
@@ -1,27 +1,42 @@
 const getStationinformation = async () => {
-  const response = await fetch(
-    "https://transport.data.gouv.fr/gbfs/nancy/station_information.json"
-  );
-  //console.log("infos : ");
-  return await response.json();
+    try {
+        const response = await fetch(
+            "https://transport.data.gouv.fr/gbfs/nancy/station_information.json"
+        );
+        if(response.ok){
+            return await response.json();
+        }
+    }catch (e) {
+        console.log(e.message);
+    }
+
 }
 
 const getStationstatus = async () => {
-    const response = await fetch(
-        "https://transport.data.gouv.fr/gbfs/nancy/station_status.json"
-    );
-    //console.log("status : ");
-    //console.log(await response.json());
-    return await response.json();
+    try {
+        const response = await fetch(
+            "https://transport.data.gouv.fr/gbfs/nancy/station_status.json"
+        );
+        if (response.ok) {
+            return await response.json();
+        }
+    }catch (e) {
+        console.log(e.message);
+        }
 }
 
 const getIncidents = async () => {
-    const response = await fetch(
-        "https://carto.g-ny.org/data/cifs/cifs_waze_v2.json"
-    );
-    //console.log("incidents : ");
-    //console.log(await response.json());
-    return await response.json();
+    try {
+        const response = await fetch(
+            "https://carto.g-ny.org/data/cifs/cifs_waze_v2.json"
+        );
+        if (response.ok) {
+            return await response.json();
+        }
+    } catch (e) {
+        console.log(e.message);
+    }
+
 }
 
 export default {
