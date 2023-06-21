d3268946cbfe8c4a5a9e4544217c78a2e88c4305
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2023-06-21 16:17:00 +0200

    fix(proxy): CORS headers

--- Java/rmi-proxy/src/main/java/fr/univlorraine/handlers/ReserverTableHandler.java
+++ Java/rmi-proxy/src/main/java/fr/univlorraine/handlers/ReserverTableHandler.java
@@ -1,17 +1,12 @@
 package fr.univlorraine.handlers;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import fr.univlorraine.ServiceProxyImpl;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.rmi.RemoteException;
-import java.sql.SQLException;
-import java.util.Scanner;
 
 import static fr.univlorraine.utils.HttpUtils.sendJSON;
 
@@ -19,20 +14,17 @@ public class ReserverTableHandler implements HttpHandler {
 
     private final ServiceProxyImpl provider;
 
-    public ReserverTableHandler (ServiceProxyImpl provider) {
+    public ReserverTableHandler(ServiceProxyImpl provider) {
         this.provider = provider;
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        // Autoriser les requêtes depuis tous les domaines
-        Headers headers = exchange.getResponseHeaders();
-        headers.add("Access-Control-Allow-Origin", "*");
-        headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-        headers.add("Access-Control-Allow-Headers", "Content-Type");
+        String response = "";
+        int statusCode = 200;
 
         String requestBody = new String(exchange.getRequestBody().readAllBytes());
-        String decodedRequestBody = URLDecoder.decode(requestBody, "UTF-8");
+        String decodedRequestBody = URLDecoder.decode(requestBody, StandardCharsets.UTF_8);
 
         String[] params = decodedRequestBody.split("&");
 
@@ -46,19 +38,20 @@ public class ReserverTableHandler implements HttpHandler {
             tel = params[3].split("=")[1];
             nomResto = params[4].split("=")[1];
             dateRes = params[5].replace('T', ' ').split("=")[1] + ":00";
-            System.out.println("date: " + dateRes);
         } else {
             System.out.println("nb argument incorrect");
         }
 
-
         try {
             provider.getGoodFoodProvider().bookTable(nomResto, nom, prenom, nbInv, tel, dateRes);
-            System.out.println("Reservation effectuée");
+            System.out.println("Réservation effectuée");
+            response = "Done!";
         } catch (Exception e) {
-            String response = "\"An error occured for book a table\";";
-            exchange.sendResponseHeaders(500, response.getBytes(StandardCharsets.UTF_8).length);
-            exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
+            response = "\"An error occured while booking a table\";";
+            statusCode = 500;
         }
+
+        sendJSON(statusCode, exchange, response);
     }
+
 }
