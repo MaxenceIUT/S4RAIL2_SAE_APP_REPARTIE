a598c6219bda96663b86070df0796e9127cda560
perez112u <perez.ioann5@etu.univ-lorraine.fr>
2023-06-21 12:02:44 +0200

    reserverTable fini normalement

--- Java/rmi-goodfood/src/main/java/fr/univlorraine/ServiceGoodFoodImpl.java
+++ Java/rmi-goodfood/src/main/java/fr/univlorraine/ServiceGoodFoodImpl.java
@@ -10,7 +10,6 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.sql.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class ServiceGoodFoodImpl implements ServiceGoodFood {
 
@@ -51,29 +50,68 @@ public class ServiceGoodFoodImpl implements ServiceGoodFood {
     @Override
     public void bookTable(String nomRestorant, String nom, String prenom, String nombreInvites, String numTelephone, String dateRes) throws SQLException, RemoteException {
 
-        Connection connexion = DBConnection.getConnexion();
-
-        PreparedStatement pst = connexion.prepareStatement("SELECT id FROM goodfood_restaurants WHERE name = ?");
-        pst.setString(1, nomRestorant);
-
-        ResultSet res = pst.executeQuery();
-        int idResto = res.getInt(1);
-
-        PreparedStatement pst2 = connexion.prepareStatement("SELECT table_id FROM goodfood_tables WHERE seat_count >= ? AND table_id NOT IN (SELECT table_id FROM goodfood_reservations WHERE reservation_date = ?)");
-        pst2.setString(1, nombreInvites);
-        pst2.setString(2, dateRes);
-
-        res = pst2.executeQuery();
-        int idTab = res.getInt(1);
+        try {
 
 
-        PreparedStatement pst3 = connexion.prepareStatement("INSERT INTO goodfood_reservations (restaurant_id, table_id, reservation_date, people_count) VALUES (?, ?, ?, ? )");
-        pst3.setInt(1, idResto);
-        pst3.setInt(2, idTab);
-        pst3.setString(3, dateRes);
-        pst3.setString(4, nombreInvites);
+            Connection connexion = DBConnection.getConnexion();
 
-        pst3.executeUpdate();
+            System.out.println("Recuperation de l'id resto");
+            PreparedStatement pst = connexion.prepareStatement("SELECT id FROM goodfood_restaurants WHERE name = ?");
+            pst.setString(1, nomRestorant);
 
+            ResultSet res = pst.executeQuery();
+            int idResto = -1;
+            if (res.next()) {
+                idResto = res.getInt("id");
+                System.out.println("id Resto : " + idResto);
+            }
+            res.close();
+            pst.close();
+
+            System.out.println("recuperation d 'une table dispo");
+            PreparedStatement pst2 = connexion.prepareStatement("SELECT table_id FROM goodfood_tables WHERE seat_count >= ? AND table_id NOT IN (SELECT table_id FROM goodfood_reservations WHERE reservation_date = ?)");
+            pst2.setInt(1, Integer.parseInt(nombreInvites));
+            pst2.setTimestamp(2, Timestamp.valueOf(dateRes));
+            int idTab = -1;
+            try {
+                ResultSet res2 = pst2.executeQuery();
+                System.out.println("requete executé");
+
+                if (res2.next()) {
+                    idTab = res2.getInt("table_id");
+                    System.out.println("table dispo: " + idTab);
+                } else {
+                    System.out.println("pas de resultat trouvé");
+                }
+
+                res2.close();
+                pst2.close();
+
+            } catch (Exception e) {
+                System.out.println("erreur requete table");
+            }
+
+
+            System.out.println("insertion dans la bdd");
+            PreparedStatement pst3 = connexion.prepareStatement("INSERT INTO goodfood_reservations (restaurant_id, table_id, reservation_date, people_count, first_name, last_name, phone_number) VALUES (?, ?, ?, ?, ?, ?, ? )");
+            pst3.setInt(1, idResto);
+            pst3.setInt(2, idTab);
+            pst3.setTimestamp(3, Timestamp.valueOf(dateRes));
+            pst3.setInt(4, Integer.parseInt(nombreInvites));
+            pst3.setString(5, prenom);
+            pst3.setString(6, nom);
+            pst3.setString(7, numTelephone);
+
+
+            pst3.executeUpdate();
+
+
+            System.out.println("inseré");
+
+            res.close();
+            pst3.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- Java/rmi-proxy/src/main/java/fr/univlorraine/handlers/ReserverTableHandler.java
+++ Java/rmi-proxy/src/main/java/fr/univlorraine/handlers/ReserverTableHandler.java
@@ -36,27 +36,28 @@ public class ReserverTableHandler implements HttpHandler {
 
         String[] params = decodedRequestBody.split("&");
 
-        String nom = null, prenom = null, nbInv = null, tel = null, nomResto = null;
+        String nom = null, prenom = null, nbInv = null, tel = null, nomResto = null, dateRes = null;
 
-        if (params.length == 5) {
-            nom = params[0];
-            prenom = params[1];
-            nbInv = params[2];
-            tel = params[3];
-            nomResto = params[4];
+        if (params.length == 6) {
+            nom = params[0].split("=")[1];
+            prenom = params[1].split("=")[1];
+            nbInv = params[2].split("=")[1];
+            tel = params[3].split("=")[1];
+            nomResto = params[4].split("=")[1];
+            dateRes = params[5].replace('T', ' ').split("=")[1];
+            System.out.println(dateRes);
         } else {
             System.out.println("nb argument incorrect");
         }
 
 
         try {
-            //provider.getGoodFoodProvider().bookTable(nomResto, nom, prenom, nbInv, tel);
+            provider.getGoodFoodProvider().bookTable(nomResto, nom, prenom, nbInv, tel, dateRes);
             System.out.println("Reservation effectuée");
         } catch (Exception e) {
             String response = "\"An error occured for book a table\";";
             exchange.sendResponseHeaders(500, response.getBytes(StandardCharsets.UTF_8).length);
             exchange.getResponseBody().write(response.getBytes(StandardCharsets.UTF_8));
         }
-        sendJSON(200, exchange, "Appelle à reservation effectué");
     }
 }
