5526c6b12a3b81181404328861464372243aaa92
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2023-06-21 11:53:59 +0200

    feat(website): added config.js and refactored data-provider (data-collector)

---
+++ website/js/data-provider.js
@@ -0,0 +1,62 @@
+import { API_URL } from "./config";
+
+const fetchJson = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      return await response.json();
+    }
+  } catch (e) {
+    throw e;
+  }
+};
+
+const getStationInformation = async () => {
+  return fetchJson(
+    "https://transport.data.gouv.fr/gbfs/nancy/station_information.json"
+  );
+};
+
+const getStationStatus = async () => {
+  return fetchJson(
+    "https://transport.data.gouv.fr/gbfs/nancy/station_status.json"
+  );
+};
+
+const getIncidents = async () => {
+  return fetchJson("https://carto.g-ny.org/data/cifs/cifs_waze_v2.json");
+};
+
+const getGoodFoodRestaurants = async () => {
+  return fetchJson(`${API_URL}/api/goodfood/restaurant`);
+};
+
+const getEtablissements = async () => {
+  return fetchJson(`${API_URL}/api/etablissements`);
+};
+
+const reserverTable = async (nom, prenom, nbInv, tel, nomResto) => {
+  let parametres = new URLSearchParams();
+  parametres.append("nom", nom);
+  parametres.append("prenom", prenom);
+  parametres.append("nbInv", nbInv);
+  parametres.append("tel", tel);
+  parametres.append("nomResto", nomResto);
+
+  return fetch(`${API_URL}/api/goodfood/reserver`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: parametres.toString(),
+  });
+};
+
+export default {
+  getEtablissements,
+  getStationInformation,
+  getStationStatus,
+  getIncidents: getIncidents,
+  getGoodFoodRestaurants,
+  reserverTable
+};
--- website/js/map.js
+++ website/js/map.js
@@ -1,77 +1,89 @@
-import data_collector from "./data_collector.js";
+import { API_URL } from "./config.js";
+import provider from "./data-provider.js";
 
-var map = L.map('map').setView([48.68895663631991, 6.178244621722987], 14);
+var map = L.map("map").setView([48.68895663631991, 6.178244621722987], 14);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
-let stationsInfoData = await data_collector.getStationinformation();
-
-let stationsStatusData = await data_collector.getStationstatus();
-
-let incidentsData = await data_collector.getIncidents();
-
-let restaurantData = await data_collector.getGoodfoodRestaurant();
-
-let etablissementsData = await data_collector.getEtablissements();
+const stationsInfoData = await provider.getStationInformation();
+const stationsStatusData = await provider.getStationStatus();
+const incidentsData = await provider.getIncidents();
+const restaurantData = await provider.getGoodfoodRestaurants();
+const etablissementsData = await provider.getEtablissements();
 
 var etablissementIcon = L.icon({
-    iconUrl: 'img/school.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+  iconUrl: "img/school.png",
+  iconSize: [40, 40], // size of the icon
+  iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, -10], // point from which the popup should open relative to the iconAnchor
 });
 
 console.log(etablissementsData);
-etablissementsData.forEach(etablissement => {
-    if(etablissement.fields.coordonnees)
-    L.marker([etablissement.fields.coordonnees[0],etablissement.fields.coordonnees[1]],{icon: etablissementIcon}).addTo(map)
-        .bindPopup(`<b>${etablissement.fields.uo_lib_officiel}</b><br> Académie : ${etablissement.fields.aca_nom}`);
-
+etablissementsData.forEach((etablissement) => {
+  if (etablissement.fields.coordonnees)
+    L.marker(
+      [
+        etablissement.fields.coordonnees[0],
+        etablissement.fields.coordonnees[1],
+      ],
+      { icon: etablissementIcon }
+    )
+      .addTo(map)
+      .bindPopup(
+        `<b>${etablissement.fields.uo_lib_officiel}</b><br> Académie : ${etablissement.fields.aca_nom}`
+      );
 });
 
 var veloIcon = L.icon({
-    iconUrl: 'img/velo.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+  iconUrl: "img/velo.png",
+  iconSize: [40, 40], // size of the icon
+  iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, -10], // point from which the popup should open relative to the iconAnchor
 });
 
-
-stationsInfoData.data.stations.forEach(station => {
-    const { station_id } = station;
-    const stationStatus = stationsStatusData.data.stations.find(station => station.station_id === station_id);
-    L.marker([station.lat, station.lon], {icon:veloIcon}).addTo(map)
-        .bindPopup(`Station ${station.name}<br> ${station.address}<br>${stationStatus.num_bikes_available} vélos disponibles<br>
+stationsInfoData.data.stations.forEach((station) => {
+  const { station_id } = station;
+  const stationStatus = stationsStatusData.data.stations.find(
+    (station) => station.station_id === station_id
+  );
+  L.marker([station.lat, station.lon], { icon: veloIcon }).addTo(map)
+    .bindPopup(`Station ${station.name}<br> ${station.address}<br>${stationStatus.num_bikes_available} vélos disponibles<br>
         ${stationStatus.num_docks_available} places de parking libres sur la station`);
 });
 
-
 var dangerIcon = L.icon({
-    iconUrl: 'img/danger.png',
-    iconSize:     [50, 50], // size of the icon
-    iconAnchor:   [25, 25], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -10] // point from which the popup should open relative to the iconAnchor
+  iconUrl: "img/danger.png",
+  iconSize: [50, 50], // size of the icon
+  iconAnchor: [25, 25], // point of the icon which will correspond to marker's location
+  popupAnchor: [0, -10], // point from which the popup should open relative to the iconAnchor
 });
 
-incidentsData.incidents.forEach(incident => {
-    const longLat = incident.location.polyline.split(' ');
-    L.marker([longLat[0], longLat[1]],{icon:dangerIcon}).addTo(map)
-        .bindPopup(`${incident.short_description}<br> Fin estimée : ${incident.endtime.split('T')[0]}`);
+incidentsData.incidents.forEach((incident) => {
+  const longLat = incident.location.polyline.split(" ");
+  L.marker([longLat[0], longLat[1]], { icon: dangerIcon })
+    .addTo(map)
+    .bindPopup(
+      `${incident.short_description}<br> Fin estimée : ${
+        incident.endtime.split("T")[0]
+      }`
+    );
 });
 
 let restaurantIcon = L.icon({
-    iconUrl: 'img/resto.png',
-    iconSize: [50, 50],
-    iconAnchor: [25, 25],
-    popupAnchor: [0, -10]
+  iconUrl: "img/resto.png",
+  iconSize: [50, 50],
+  iconAnchor: [25, 25],
+  popupAnchor: [0, -10],
 });
 
-restaurantData.forEach(resto => {
-    const marker = L.marker([resto.latitude, resto.longitude], {icon: restaurantIcon}).addTo(map)
-        .bindPopup(`${resto.nom}<br>
+restaurantData.forEach((resto) => {
+  const marker = L.marker([resto.latitude, resto.longitude], {
+    icon: restaurantIcon,
+  }).addTo(map).bindPopup(`${resto.nom}<br>
 <div id="form">
     <label for="nom">Nom :</label>
     <input type="text" id="nom">
@@ -88,40 +100,21 @@ restaurantData.forEach(resto => {
     <button id="bouttonReserve" type="submit" data-nomResto="${resto.nom}">Un simple bouton</button>
 </div>`);
 
+  marker.on("popupopen", (e) => {
+    const popup = e.popup;
+    const button = popup.getElement().querySelector("#bouttonReserve");
+    button.addEventListener("click", (event) => {
+      let nom = document.getElementById("nom").value;
+      let prenom = document.getElementById("prenom").value;
+      let nbInv = document.getElementById("nbInv").value;
+      let tel = document.getElementById("tel").value;
+      let nomResto = event.target.getAttribute("data-nomResto");
 
-    marker.on('popupopen', (e) => {
-        const popup = e.popup;
-        const button = popup.getElement().querySelector("#bouttonReserve");
-        button.addEventListener("click", (event) => {
-            let nom = document.getElementById('nom').value;
-            let prenom = document.getElementById('prenom').value;
-            let nbInv = document.getElementById('nbInv').value;
-            let tel = document.getElementById('tel').value;
-            let nomResto = event.target.getAttribute('data-nomResto');
-
-            if (nom !== '' && prenom !== '' && nbInv != null && tel !== '') {
-                let parametres = new URLSearchParams();
-                parametres.append('nom', nom);
-                parametres.append('prenom', prenom);
-                parametres.append('nbInv', nbInv);
-                parametres.append('tel', tel);
-                parametres.append('nomResto', nomResto);
-
-                fetch('http://localhost:8080/api/goodfood/reserver', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: parametres.toString()
-                });
-
-            } else {
-                console.log("Parametres incomplets");
-            }
-        });
-    })
+      if (nom !== "" && prenom !== "" && nbInv != null && tel !== "") {
+        provider.reserverTable(nom, prenom, nbInv, tel, nomResto);
+      } else {
+        console.log("Paramètres incomplets");
+      }
+    });
+  });
 });
-
-
-
-
